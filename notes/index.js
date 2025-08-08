cc2ed7ba937d93fa4f048cc0732c3ae7bcc9eccc
leontoys <svliyon@gmail.com>
2025-08-09 07:09:07 +1000

    12.3 sql query works - removed ssl for now

--- notes/index.js
+++ notes/index.js
@@ -1,26 +1,21 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, QueryTypes } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
-});
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 const main = async () => {
   try {
-    await sequelize.authenticate()
+    await sequelize.authenticate();
 
-    const notes = await sequelize.query("SELECT * FROM notes", { type: QueryTypes.SELECT })
-    console.log(notes)
-    sequelize.close()
+    const notes = await sequelize.query("SELECT * FROM notes", {
+      type: QueryTypes.SELECT,
+    });
+    console.log(notes);
+    sequelize.close();
   } catch (error) {
-    console.error('Unable to connect to the database:', error)
+    console.error("Unable to connect to the database:", error);
   }
-}
+};
 
-main()
+main();
