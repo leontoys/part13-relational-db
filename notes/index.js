01e7a5cf30587d26fd40e84ce101aff792fa2cf6
leontoys <svliyon@gmail.com>
2025-08-09 07:12:52 +1000

    13 node app with postgres

--- notes/index.js
+++ notes/index.js
@@ -1,21 +1,25 @@
 require("dotenv").config();
-
 const { Sequelize, QueryTypes } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const express = require("express");
+const app = express();
 
-const main = async () => {
-  try {
-    await sequelize.authenticate();
+const sequelize = new Sequelize(process.env.DATABASE_URL)//, {
+/*dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});*/
 
-    const notes = await sequelize.query("SELECT * FROM notes", {
-      type: QueryTypes.SELECT,
-    });
-    console.log(notes);
-    sequelize.close();
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-};
-
-main();
+app.get("/api/notes", async (req, res) => {
+  const notes = await sequelize.query("SELECT * FROM notes", {
+    type: QueryTypes.SELECT,
+  });
+  res.json(notes);
+});
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
