fc5c898ff5a08a5eee00a3e66b497f47a9e79bb1
leontoys <svliyon@gmail.com>
2025-08-10 08:30:17 +1000

    12.13 case insensitive search

--- blogs/controllers/blogs.js
+++ blogs/controllers/blogs.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const { Blog, User } = require("../models");
 const { SECRET } = require("../util/config");
 const jwt = require("jsonwebtoken");
+const { Op } = require("sequelize");
 
 
 const blogFinder = async (req, res, next) => {
@@ -15,10 +16,19 @@ const blogFinder = async (req, res, next) => {
 };
 
 router.get("/", async (req, res) => {
+  const where = {}
+
+  if (req.query.search) {
+    where.title = {
+      [Op.iLike] : `%${req.query.search}%`
+    }
+  }
+  console.log(where)
   const blogs = await Blog.findAll({
     include: {
       model : User
-    }
+    },
+    where
   });
   res.json(blogs);
 });
