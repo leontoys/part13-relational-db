af5907ea5164daf8fa57ade35617ddbae70a2d36
leontoys <svliyon@gmail.com>
2025-08-10 09:26:31 +1000

    13.14 search in either title or author field

--- blogs/controllers/blogs.js
+++ blogs/controllers/blogs.js
@@ -16,12 +16,15 @@ const blogFinder = async (req, res, next) => {
 };
 
 router.get("/", async (req, res) => {
-  const where = {}
+  let where = {}
 
   if (req.query.search) {
-    where.title = {
-      [Op.iLike] : `%${req.query.search}%`
-    }
+    where = {
+      [Op.or]: [
+        { title: { [Op.iLike]: `%${req.query.search}%` } },
+        { author: { [Op.iLike]: `%${req.query.search}%` } }
+      ],
+    };
   }
   console.log(where)
   const blogs = await Blog.findAll({
